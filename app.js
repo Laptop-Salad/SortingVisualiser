3a9e18a9a6e15feaa799a95be2c7a87be27d6dd0
Amanda <squaretacos456@gmail.com>
2022-06-06 18:45:02 +0100

    add randomise button

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var n = 20;
+var n = 100;
 var arr = [];
 
 function generateDivs() {
     for (var i in arr) {
         var newElem = document.createElement('div');
         newElem.id = i;
-        newElem.innerHTML = arr[i];
         newElem.className = "bg-light w-25 m-1";
         var divHeight = (arr[i] * 8) + 10;
         newElem.style.height = divHeight+"px";
@@ -13,11 +12,19 @@ function generateDivs() {
     }
 }
 
+function removeDivs() {
+    for (var i in arr) {
+        var elem = document.getElementById(i);
+        elem.remove();
+    }
+
+    createArray();
+}
+
 function createArray() {
     for (var i = 0; i < n; i++) {
         const randomNum = Math.floor(Math.random() * 100) + 1;;
         arr[i] = randomNum;
-        console.log(arr[i]);
     }
 
     generateDivs();
